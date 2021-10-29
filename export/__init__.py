8332abec85244a38f66f58adea69b96ee73b6a5a
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2021-10-29 10:51:04 +0100

    Separate package functionality

--- export/__init__.py
+++ export/__init__.py
@@ -1,20 +1,4 @@
-import sys
-import types
-import typing
+from . import utils
+from .exporter import export
 
-class ExportModule(types.ModuleType):
-    @staticmethod
-    def export(obj):
-        module: types.ModuleType = sys.modules[obj.__module__]
-
-        if not hasattr(module, '__all__'):
-            module.__all__: typing.List[str] = []
-
-        # TODO: Remove duplicates
-        module.__all__.append(obj.__name__)
-
-        return obj
-
-    __call__ = export
-
-sys.modules[__name__].__class__ = ExportModule
+utils.make_callable(__name__, export)
